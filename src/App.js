4f038f91d58a89e1558ef8b55066c20953185ed6
Gillian <105639251+MaronillaG@users.noreply.github.com>
2023-08-11 14:21:27 +0200

    removing bloat from create react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Quote from './Quote';
 import './App.css';
 
 function App() {
+  const [quote, setQuote] = useState({quote: '', author: ''});
+  const [quotesData, setQuotesData] = useState([]); 
+  const generateRandomIndex = (max) => {
+    return Math.floor(Math.random() * max);
+  };
+
+
+  useEffect(() => {
+    fetch('/proxy')
+    .then( response => response.json())
+    .then (data => {
+      setQuotesData(data);
+      const randomIndex = generateRandomIndex(data.length)
+      console.log(data[randomIndex].q,data[randomIndex].a)
+      setQuote({
+        quote: data[randomIndex].q,
+        author: data[randomIndex].a
+      })
+    })
+    .catch(error => {
+      console.error('Error fetching quotes:', error);
+    })
+  }, []);
+  
+const generateRandomQuote = () => {
+  const randomIndex = generateRandomIndex(quotesData.length);
+  setQuote({
+    quote : quotesData[randomIndex].q,
+    author: quotesData[randomIndex].a
+  })
+}
+
+const [backgroundImage, setBackgroundImage] = useState('');
+
+  useEffect(() => {
+    fetch('/background-image')
+      .then(response => response.json())
+      .then(data => {
+        setBackgroundImage(data.backgroundImage);
+      })
+      .catch(error => {
+        console.error('Error fetching background image:', error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundImage: `url(${backgroundImage})`}}>
+
+      <Quote text={quote.quote} author={quote.author} generateRandomQuote={generateRandomQuote} />
     </div>
   );
 }
